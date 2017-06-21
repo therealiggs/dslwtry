1a4e48aa266db7cdbf9f94174c106e7914fbbc59
Igor Sher <work.iggs@gmail.com>
2017-06-21 18:28:36 +0300

    Initial commit

---
+++ app/src/main/java/iggs/dslwtry/MainActivity.java
@@ -0,0 +1,84 @@
+package iggs.dslwtry;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int c = 0;
+    private final String tag = "dslw";
+    private String word;
+    int[] array = new int[50];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+        Log.d(tag,"created");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for(int i=0;i<50;i++){
+            array[i] = i;
+        }
+
+
+        Random rnd = new Random();
+        for(int i=array.length-1;i>0;i--){
+            int index = rnd.nextInt(i+1);
+            int a = array[index];
+            array[index] = array[i];
+            array[i] = a;
+        }
+
+        for(int i=0;i<50;i++){
+           Log.d(tag,array[i]+"");
+        }
+
+
+        Resources res = getResources();
+        final TypedArray words = res.obtainTypedArray(R.array.words);
+        final Chronometer chrono = (Chronometer)findViewById(R.id.chrono);
+        Button dSLW =(Button)findViewById(R.id.DSLW);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        dSLW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                long elapsedMillis = SystemClock.elapsedRealtime()
+                        - chrono.getBase();
+                Log.d(tag,"clicked");
+                if (c == 0){
+                    chrono.start();
+                }
+                if (c == 50 | elapsedMillis > 60000 ) {
+                    chrono.stop();
+                    textView.setText("Ход следующего игрока, ваш счёт: " +c+", ваше время: "+(int)(elapsedMillis/1000) + " секунд(ы)");
+                    c = 0;
+                    chrono.setBase(SystemClock.elapsedRealtime());
+                }
+
+                else {
+                    Log.d(tag, "clicked");
+                    word = words.getString(array[c]);
+                    textView.setText(word);
+                    c++;
+                }
+            }
+
+
+        });
+    }
+}
